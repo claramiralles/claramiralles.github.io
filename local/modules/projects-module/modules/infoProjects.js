2ba2a9b34d7a64912b217066f9a34dc040e497fc
Clara Miralles <clara.miralles@epiphany-rbc.com>
2020-05-23 16:59:43 +0200

    WIP home, projects and contact

---
+++ local/modules/projects-module/modules/infoProjects.js
@@ -0,0 +1,57 @@
+const infoProjects = {
+    technicalSkills: ['react', 'javascript', 'python', 'sass', 'less', 'css3', 'html5', 'gulp', 'gsap', 'php'],
+    scrumSkills: ['agile', ' scrum', 'git', 'github', 'trello', 'slack', ],
+    softwareSkills: ['visual studio code', 'zeplin', 'facebook adds', 'google analytics'],
+    projects: [
+        {
+            id: 1,
+            name: 'Your Calls 2.0',
+            description: 'SPA built in React to record information about client calls. Data visualisation for analysis purposes. Shown in graphics done by integrating React Google Charts. Web layout with HTML5 & CSS3.',
+            urlGithub: 'https://github.com/claramiralles/your-calls-2.0-react-and-react-google-charts',
+            urlDemo: 'http://beta.adalab.es/fairfax-m4-your-calls-app',
+            image: require('../img/projects/img_your_calls.png'),
+        },
+        {
+            id: 2,
+            name: 'Harry Potter Charaters Search Engine',
+            description: 'SPA built in React components, with base in JavaScript. Web routing with React Dom. Web layout with HTML5 & CSS3. Built individually within 3 days as my final evaluation of React at ADALAB.',
+            urlGithub: 'https://github.com/Adalab/harry-potter-characters-search-engine-react',
+            urlDemo: 'http://beta.adalab.es/harry-potter-characters-search-engine-react/',
+            image: require('../img/projects/img_harry_potter.png'),
+        },
+        {
+            id: 3,
+            name: 'Rock & code',
+            description: 'Website built in HTML & CSS, following responsive design principles. Use of flexbox and grid as web layout systems. Built by a 5-developers-team, following agile principles & scrum methodologies.',
+            urlGithub: 'https://github.com/claramiralles/rock-and-code-team-website-html-css',
+            urlDemo: 'http://beta.adalab.es/f-m1-rock-code/',
+            image: require('../img/projects/img_rock_and_code.png'),
+        },
+        {
+            id: 4,
+            name: 'Awesome Profile Cards',
+            description: 'WebApp built in JavaScript to create your own business card. Customisable themes. Required fields. Links to social media. Local storage. Ready to share on Twitter. Web layout with HTML5 & CSS3.',
+            urlGithub: 'https://github.com/claramiralles/awesome-profile-cards-javascript',
+            urlDemo: 'http://beta.adalab.es/fairfax-NaN-NotaName-/.',
+            image: require('../img/projects/img_awesome_profile_cards_js.png'),
+        },
+        {
+            id: 5,
+            name: 'Awesome Profile Cards «legacy code»',
+            description: 'SPA built in React (migration from legacy code in JavaScript). Web routing with React Dom. Web layout with HTML & CSS. Built by a 5-developers-team, following agile principles & scrum methodologies.',
+            urlGithub: 'https://github.com/claramiralles/awesome-profile-cards-react',
+            urlDemo: 'http://beta.adalab.es/f-m3-codebusters/',
+            image: require('../img/projects/img_awesome_profile_cards_react.png'),
+        },
+        {
+            id: 6,
+            name: 'Pokemon Cards Game',
+            description: 'WebApp built in JavaScript. Responsive design with HTML & CSS. Built individually in 3 days as my final evaluation of JavaScript at ADALAB.',
+            urlGithub: 'https://github.com/Adalab/pokemon-cards-game-javascript',
+            urlDemo: 'http://beta.adalab.es/pokemon-cards-game-javascript/',
+            image: require('../img/projects/img_pokemon_cards_game.png'),
+        },
+    ]   
+};
+
+export { infoProjects };
---
+++ local/modules/projects-module/projects.js
@@ -0,0 +1,1 @@
+import * as InfoProjects from './modules/infoProjects.js';
